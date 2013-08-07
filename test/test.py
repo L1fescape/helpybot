2fb994d8a9e1b5c5c67bd63d5f91e959f312718f
Andrew Kennedy <akennedy2292@gmail.com>
2013-08-07 15:27:15 -0400

    fixed comments on test

--- test/test.py
+++ test/test.py
@@ -1,9 +1,11 @@
-# packages
+# import helpybot
 import os, sys
 parentdir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 os.sys.path.insert(0,parentdir) 
 from helpybot import HelpyBot
 
+# create dummy tweet class (I don't know why you need to do this to
+# set attributes on a dict via `my_dict.my_attr = 'asdf'`)
 class Tweet(dict):
   pass
 
